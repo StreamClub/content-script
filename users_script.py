6e5f66a8734925658836ab815b7c6c4b9086b417
dbalestieri <dbalestieri@mulesoft.com>
2024-02-18 20:41:25 -0300

    parametrize number of recomendations

--- users_script.py
+++ users_script.py
@@ -12,18 +12,18 @@ def find_n_closest_to_user(df, user, n=5):
     closest_distances = similarities[closest_indices]
     result_df = pd.DataFrame({'id': closest_ids.values, 'distance': closest_distances})
     result_df = result_df.sort_values(by='distance', ascending=False)
-    print(result_df)
     return result_df['id'].values
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python3 users_script.py user_id")
+    if len(sys.argv) != 3:
+        print("Uso: python3 users_script.py user_id n_recomendations")
         sys.exit(1)
     user_id = sys.argv[1]
+    n = int(sys.argv[2])
     (movies_ids, _) = get_user_seen(user_id)
     df = pd.read_csv("./movies.csv")
     filtered_df = df[df['id'].isin(movies_ids)]
     user_array = filtered_df.drop(['id'], axis=1).sum().values
     user_array = [x / 3 for x in user_array]
-    recomendations = find_n_closest_to_user(df[~df['id'].isin(movies_ids)], user_array, 10)
+    recomendations = find_n_closest_to_user(df[~df['id'].isin(movies_ids)], user_array, n)
     print(recomendations)

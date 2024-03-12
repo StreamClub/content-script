6986315bd4ba07a9d420e23af81c6afd78f67bb2
dbalestieri <dbalestieri@mulesoft.com>
2024-03-12 15:05:41 -0300

    fix flat issue

--- movies.py
+++ movies.py
@@ -5,5 +5,5 @@ def get_movie_details_for_id(ids):
   details = []
   for id in ids:
     details.append(get_movie_details(id))
-  df = pd.json_normalize(details)
+  df = pd.DataFrame(details)
   return df
